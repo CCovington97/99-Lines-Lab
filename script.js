4819e80dce10406d1e609a5dc7991ab1f37a575f
Connor <ccovington439@gmail.com>
2020-01-15 10:31:17 -0600

    finished project

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 var friends = ['Calvin', 'Joshua', 'Mariah', 'Michael', 'Ryan'];
 
+for (var i = 0; i < friends.length; i++) {
+    for (var j = 99; j > 0; j--) {
+        var k = j - 1;
+        if (j === 1) {
+            console.log(j + ' line of code in the file, ' + j + ' line of code, ' + friends[i] + ' strikes one out, clears it all out, ' + k + ' lines of code in the file.');
+        } else if (j === 2) {
+            console.log(j + ' lines of code in the file, ' + j + ' lines of code, ' + friends[i] + ' strikes one out, clears it all out, ' + k + ' line of code in the file.');
+        } else {
+            console.log(j + ' lines of code in the file, ' + j + ' lines of code, ' + friends[i] + ' strikes one out, clears it all out, ' + k + ' lines of code in the file.');
+        }
+    }
+}
